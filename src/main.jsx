c1ce3ae60db7e4ddc07beba152b89889ec017bd4
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-05-31 11:54:52 +0600

    home page remove

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux'
 import store from './store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
   ,
 )
