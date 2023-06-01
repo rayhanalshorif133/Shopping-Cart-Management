e3872115c7692a2cae30ab5ff2bee817db0ccffd
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-06-01 17:33:16 +0600

    hide contact

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -51,9 +51,9 @@ export default function Navbar() {
                                 <Link to="/about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                                     About</Link>
                             </li>
-                            <li>
+                            {/* <li>
                                 <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                            </li>
+                            </li> */}
                         </ul>
                     </div>
                 </div>
