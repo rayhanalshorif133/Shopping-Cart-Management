3c24d9a15bcc40dc3cbe05341c00582d7b919396
rayhanalshorif133 <rayhanalshorif@gmail.com>
2023-06-03 00:05:56 +0600

    header_nav response done

--- src/features/products/card/CardView.jsx
+++ src/features/products/card/CardView.jsx
@@ -26,23 +26,21 @@ export default function CardView(props) {
 
 
     return (
-        <div key={product.id}>
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div key={product.id} className="product_card">
+            <a href="#">
+                <img className="product_image rounded-t-lg product__image text-center justify-center mx-auto mt-2" src={product.image} alt="" height="100px" width={400} />
+            </a>
+            <div className="p-5">
                 <a href="#">
-                    <img className="product_image rounded-t-lg product__image text-center justify-center mx-auto mt-2" src={product.image} alt="" height="100px" width={400} />
+                    <h5 className="mb-1 text-sm font-bold tracking-tight text-gray-900 dark:text-white">
+                        {product.title}
+                    </h5>
                 </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-1 text-sm font-bold tracking-tight text-gray-900 dark:text-white">
-                            {product.title}
-                        </h5>
-                    </a>
-                    <p className="mb-2 font-normal text-gray-700 dark:text-gray-400">
-                        {product.category}
-                    </p>
-                    <div className="add_to_cart_btn" onClick={handleAddToCart}>
-                        <FaPlusSquare className='mr-2' /> Add to cart
-                    </div>
+                <p className="mb-2 font-normal text-gray-700 dark:text-gray-400">
+                    {product.category}
+                </p>
+                <div className="add_to_cart_btn" onClick={handleAddToCart}>
+                    <FaPlusSquare className='mr-2' /> Add to cart
                 </div>
             </div>
         </div>
