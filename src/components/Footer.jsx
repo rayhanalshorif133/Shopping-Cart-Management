ce72a11fe3344aae96697f613e91a1716891a7ba
rayhanalshorif133 <rayhanalshorif@gmail.com>
2023-06-03 00:53:33 +0600

    product curd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
@@ -34,7 +34,7 @@ export default function Footer() {
                     <div className="sm:flex sm:items-center sm:justify-between">
                         <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
                             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            <span className="self-center font-semibold whitespace-nowrap dark:text-white">
                                 Cart App
                             </span>
                         </a>
